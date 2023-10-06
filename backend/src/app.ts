18648a184d16699de9e2921a6d37ae53492f8fa9
Juliano Santos <julianosantosdev@outlook.com>
2023-10-06 11:01:08 -0300

    feat: front end structure and login feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,9 +5,12 @@ import userRoutes from './routes/users.routes'
 import handleError from './middlewares/handleAppError.middleware'
 import loginRoute from './routes/login.routes'
 import taskRoute from './routes/tasks.routes'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/users', userRoutes)
 app.use('/login', loginRoute)
